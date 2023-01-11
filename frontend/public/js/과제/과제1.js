0680722d61b348645825bbb55e97f4eb7dcfdfa0
kimy041 <miyeong0418@gmail.com>
2023-01-11 11:32:14 +0900

    Day09

--- frontend/public/js/과제/과제1.js
+++ frontend/public/js/과제/과제1.js
@@ -82,7 +82,7 @@ function save(){
 		document.querySelector('.resultBox2').innerHTML = '이미 등록된 학번입니다.'}
 	else if(sIndex2 == -1 && snoValue2.length == 8){
 		studentArray.push(snoValue2); 
-		sno2.valeu = '';
+		sno2.value = '';
 		document.querySelector('.resultBox2').innerHTML = '등록되었습니다.'}
 	else if(snoValue2.length != 8 && snoValue2 != '' ){
 		document.querySelector('.resultBox2').innerHTML = '8자리로 입력해주세요'}
@@ -92,23 +92,52 @@ function save(){
 	console.log(studentArray)	
 }
 
-//3. 요휴성검사[ 중복체크 ]
-	//입력받은 값[snoValue2]이 있고 기존에 있는 값들[studentArray]과 비교
-	// 경우의수 2가지[중복이 있다 or 없다]
-/*	let resultBox2 = document.querySelector('.resultBox2')
+
+/*	
+function onAdd(){ // 1. 함수 시작 
+
+		console.log('1.함수 실행') // 1. 확인
 	
-	 if(studentArray.indexOf( snoValue2 ) == -1 ){ //중복 없다
-		console.log('학번 등록했습니다.')
-		studentArray.push(snoValue2)
-		
-			console.log( resultBox2 )
-			resultBox2.innerHTML = '학번 등록되었습니다.'
+	// 2. <input> 마크업 가져와서 변수에 저장 [ Dom 객체 ]
+	let sno2 = document.querySelector('.sno2')
+	
+		console.log( sno2 ) //2. 확인
+	
+	// 2.2 <input> 마크업 에 입력된 value[값] 가져와서 변수에 저장 
+	let sno2Value = sno2.value
+	
+		console.log('2. 입력값 : ' + sno2Value ) //2. 확인
+	
+	// ! 검사 변수 [ 유효성검사 체크리스트 ]
+	let confirm = 0;
+	// ! 추가코드 1. 만약에 입력된 값이 공백이면 
+	if( sno2Value == '' ){ console.log('공백이네요.~'); confirm++; }
+	// ! 추가코드 2. 만약에 입력된 값이 8자리 아니면 
+		console.log('2. 입력값 길이 : ' + sno2Value.length );
+	if( sno2Value.length !=8 ){ console.log('8자리 아니네요~'); confirm++; }
+	// 3. 유효성검사[ 중복체크 ]
+		// 입력받은 값[sno2Value]이 있고 기존에 있는 값들[studentArray] 과 비교
+		// 경우의수 2가지 [ 중복이 있다 or 없다 ] 
+	// 지역변수 : 해당 지역{ } 에서 선언되면 { } 안으로 이동가능 / { } 밖으로 이동 불가능 
+	// if{} 와 else {} 에서 같이 사용할 변수 이기에 if 밖에 선언 
+	let resultBox2 = document.querySelector('.resultBox2') 
+	
+	if( confirm == 0 ){ // 만약에 confirm 0 일때만 아래 코드  실행
+		if( studentArray.indexOf(sno2Value) == -1 ){ // 중복 없다 
+				console.log('학번 등록했습니다.')
+			studentArray.push( sno2Value )
+				console.log( resultBox2 )
+			resultBox2.innerHTML = '학번 등록했습니다.'
+			// !추가코드 3. 만약에 등록을 성공했으면 <input> value 초기화
+			sno2.value = ''
+		} // if end 
+		else{ // 중복 있다.
+			console.log('이미 등록된 학번 입니다.') 
+			resultBox2.innerHTML = '이미 존재하는 학번입니다..' // resultBox2 is not defined
+		}// else end 
 	}
-		else{//중복 있다.
-		console.log('이미 등록된 학번입니다.')
-			resultBox2.innerHTML = '이미 존재하는 학번입니다.'
-		
-	}//else end
+} // 함수 끝
+
 */
 
 
