e450b9c514c6df1602b997053fb2b716ac09d874
kimy <kimy@218.38.137.27>
2023-01-11 19:11:14 +0900

    Day09: H

--- frontend/public/js/과제/과제3.js
+++ frontend/public/js/과제/과제3.js
@@ -1,7 +1,7 @@
 
 let contentArray = []
 //let 테이블행 = '<tr> <th>번호</th><th>방문록</th> </tr>';
-//for( 초기값 ; 배열.length ; 증감식 ){ 실행문 } let j = 0 ; j < 과일상자.length ; j++
+//for( 초기값 ; 배열.length ; 증감식 ){ 실행문 } 
 function addContent(){
 	let text = document.querySelector('.text').value
 	contentArray.push( text )
@@ -9,8 +9,9 @@ function addContent(){
 	let 테이블행 = '<tr> <th>번호</th><th>방문록</th> </tr>';
 	for(let n = 1 ; n <= contentArray.length ; n++ ){
 		
-		테이블행 += '<tr> <th>'+n+'</th><th>'+contentArray+'</th> </tr>';
+		테이블행 += '<tr> <th>'+n+'</th><th>'+contentArray[n-1]+'</th> </tr>';
 	}
 	
 	document.querySelector('.content_table').innerHTML = 테이블행
+	document.querySelector('.text').value = ''
 }
