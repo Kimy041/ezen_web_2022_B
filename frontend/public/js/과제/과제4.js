f8c4aa697920146d116de3c2027fbc8cccd1f16c
kimy <kimy@localhost>
2023-01-16 00:23:00 +0900

    H

--- frontend/public/js/과제/과제4.js
+++ frontend/public/js/과제/과제4.js
@@ -1,28 +1,55 @@
 
 let 도서목록 = [ '혼자공부하는자바' , '이것이자바다' , '열형 C언어' ]
 let 대여목록 = [ '혼자공부하는자바' ]
+
 	
 let html = '<tr><th>번호</th><th>도서</th><th>도서대여여부</th><th>비고</th></tr>'
-
-for(let i = 0 ; i < 도서목록.length ; i++ ){
-	html += `<tr><td>${i+1}</td><td>${도서목록[i]}</td><td><div class="btn"></div></td><td><button onclick="get()">대여버튼</button><button onclick="">반납버튼</button></td></tr>`
-				}
+for(let i = 0 ; i < 도서목록.length ; i++ ){// for s // 고객페이지 도서목록 출력
+	html += `<tr>
+				<td>${i+1}</td>
+				<td>${도서목록[i]}</td>
+				<td class="YorN"></td>
+				<td>
+					<button onclick="bookGet(${i})">대여버튼</button>
+					<button onclick="bookReturn(${i})">반납버튼</button>
+				</td>
+			</tr>`
+			
+	}// for e
 document.querySelector('.customerTable').innerHTML = html
+console.log(html)
 
-function 버튼클릭( 번호 ){// f s
-	console.log( 번호+'번호를 선택했군요.')
-	// 1. 중복검사/취소	[배열명.indexOf(찾을데이터) : 찾은 데이터의 인덱스번호 반환 , 없으면 -1 ]
-	if(선택번호목록.indexOf(번호) >= 0 ){// if s
-		alert('대여중인 도서입니다.')
-		추첨버튼출력()// 함수호출
-		return; // 반환값은 없지만 함수를 끝낼 수 있다. [ 더이상 아래로 코드가 실행되지 않는다. ]
-	}// if e
-	// 2. 6개까지만 저장 [ 배열명.length : 배열내 데이터 총 개수 ]
-	if( 선택번호목록.length == 6 ){// if s //배열내 숫자가 6개 존재하면
-		alert('이미 모두[6개]를 선택했습니다.')
-		return;
-	}// if e
-	// 위 2가지 유효성검사를 충족하지 않았을때 저장
-	선택번호목록.push(번호)
-	추첨버튼출력();// 함수호출	
+function 도서대여여부( YorN ){
+	if(대여목록.indexOf(도서목록[YorN]) == -1 ){ // if s //대여목록에 도서목록에 있는 도서가 존재하지않는다면
+		document.querySelector('.YorN').innerHTML='대여가능'
+	}else{
+		document.querySelector('.YorN').innerHTML='대여중' 
+		}// if e
+}
+
+
+function bookGet( gbtn ){// f s
+	if(대여목록.indexOf(도서목록[gbtn]) == -1 ){ // if s //대여목록에 도서목록에 있는 도서가 존재하지않는다면
+		alert('대출되었습니다.')
+		대여목록.push(도서목록[gbtn]) // 대여된 도서 대여목록에 추가
+		console.log(대여목록)
+	}else{
+		alert('이미 대여중인 도서입니다.') 
+		}// if e
+	도서대여여부()
 }// f e
+
+function bookReturn(rbtn ){// f s
+	if(대여목록.indexOf(도서목록[rbtn]) !== -1 ){// if s //대여목록에 도서목록에 있는 도서가 존재하다면
+		alert('반납되었습니다.')
+		대여목록.splice( 대여목록.indexOf(도서목록[rbtn]) , 1 )// 반납된 도서 대여목록에서 제거
+		console.log(대여목록)
+	}else{
+		alert('대출된 도서가 아닙니다.') 
+		} // if e
+	도서대여여부()
+}// f e
+
+
+
+
