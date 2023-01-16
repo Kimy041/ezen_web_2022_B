e68a8df739a68a885857c702ea7138f2f3d805ed
kimy041 <miyeong0418@gmail.com>
2023-01-16 18:08:29 +0900

    과제

--- frontend/public/js/과제/과제4.js
+++ frontend/public/js/과제/과제4.js
@@ -2,29 +2,30 @@
 let 도서목록 = [ '혼자공부하는자바' , '이것이자바다' , '열형 C언어' ]
 let 대여목록 = [ '혼자공부하는자바' ]
 
-	
-let html = '<tr><th>번호</th><th>도서</th><th>도서대여여부</th><th>비고</th></tr>'
-for(let i = 0 ; i < 도서목록.length ; i++ ){// for s // 고객페이지 도서목록 출력
-	html += `<tr>
-				<td>${i+1}</td>
-				<td>${도서목록[i]}</td>
-				<td class="도서대여여부${i}" ></td>
-				<td>
-					<button onclick="bookGet(${i})">대여버튼</button>
-					<button onclick="bookReturn(${i})">반납버튼</button>
-				</td>
-			</tr>`
-			
-	}// for e
-document.querySelector('.customerTable').innerHTML = html
-console.log(html)
+출력()
+function 출력(){
+	let html = '<tr><th>번호</th><th>도서</th><th>도서대여여부</th><th>비고</th></tr>'
+	for(let i = 0 ; i < 도서목록.length ; i++ ){// for s // 고객페이지 도서목록 출력
+		html += `<tr>
+					<td>${i+1}</td>
+					<td>${도서목록[i]}</td>
+					<td><div class="도서대여여부">${도서대여여부( i )}</div></td>
+					<td>
+						<button onclick="bookGet(${i})">대여버튼</button>
+						<button onclick="bookReturn(${i})">반납버튼</button>
+					</td>
+				</tr>`
+				
+		}// for e
+	document.querySelector('.customerTable').innerHTML = html
+	console.log(html)
+}
 
-
-function 도서대여여부( YorN ){
-	if(대여목록.indexOf(도서목록[YorN]) == -1 ){ // if s //대여목록에 도서목록에 있는 도서가 존재하지않는다면
-		'대여가능'
+function 도서대여여부( i ){
+	if(대여목록.indexOf(도서목록[i]) == -1 ){ 
+		return '대여가능'
 	}else{
-		'대여중' 
+		return '대여중' 
 		}// if e
 }
 
@@ -37,8 +38,7 @@ function bookGet( gbtn ){// f s
 	}else{
 		alert('이미 대여중인 도서입니다.') 
 		}// if e
-	도서대여여부()
-	document.querySelector('.YorN').innerHTML='대여중' 
+		출력()
 }// f e
 
 function bookReturn( rbtn ){// f s
@@ -49,8 +49,7 @@ function bookReturn( rbtn ){// f s
 	}else{
 		alert('대출된 도서가 아닙니다.') 
 		} // if e
-	도서대여여부()
-	document.querySelector('.YorN').innerHTML='대여가능'
+		출력()
 }// f e
 
 
