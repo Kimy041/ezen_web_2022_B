228132f9752408bca8bc258e7272c733d1aa089f
kimy041 <miyeong0418@gmail.com>
2023-02-13 13:17:06 +0900

    Day29

---
+++ backend/src/Day09/Ex4_p239/실행.java
@@ -0,0 +1,15 @@
+package Day09.Ex4_p239;
+
+public class 실행 {
+	public static void main(String[] args) {
+		
+		Calculator myCalcu = new Calculator();
+		
+		double result1 = myCalcu.areaReactangle(10);
+		
+		double result2 = myCalcu.areaReactangle(10 , 20);
+		
+		System.out.println( result1 );
+		System.out.println( result2 );
+	}
+}
