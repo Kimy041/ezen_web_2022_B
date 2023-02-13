3877a3e3d70053a257951361c8f5059e82543c85
kimy041 <miyeong0418@gmail.com>
2023-02-13 15:59:04 +0900

    Day29

---
+++ backend/src/Day09/Ex10_게시판/view/Front.java
@@ -0,0 +1,36 @@
+package Day09.Ex10_게시판.view;
+
+import java.util.Scanner;
+
+public class Front {
+	
+	// 1. 싱글톤 객체 [ 1. 프로그램내 하나의 객체 - 공유 메모리 ]
+	private static Front front = new Front();
+	private Front() {}
+	public static Front getInstance() {
+		return front;
+	}
+	
+	// 필드 : 입력 객체
+	private Scanner scanner = new Scanner(System.in);
+	
+	// 2. index 함수
+	public void index() {
+		while(true) {
+			System.out.println(" 1.쓰기 : ");
+			int ch = scanner.nextInt();
+			if( ch == 1 ) { writer_page(); }
+		}
+
+	}
+	
+	// 3. 쓰기 페이지 함수
+	private void writer_page() {
+		System.out.println("--------글 쓰기 페이지----------");
+	}
+	
+	// 4. 출력 페이지 함수
+	private void print_page() {
+		
+	}
+}
